5392ded2ddb803de2f296557d940f7a4d2a6e2d4
LinchSmyth <andrei95-1995@mail.ru>
2016-07-21 20:58:52 +0300

    Add Regexp to validate e-mail, improve validation of age

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,7 +1,10 @@
 class Feedback < ActiveRecord::Base
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
   validates :name,      presence: true
-  validates :age,       presence: true
-  validates :email,     presence: true
+  validates :age,       presence: true, inclusion:  { within: 17..65, message: "is invalid: you must be from 17 to 65 years old" }
+  validates :email,     presence: true, format:     { with: VALID_EMAIL_REGEX }
   validates :datetime,  presence: true
 
 end
