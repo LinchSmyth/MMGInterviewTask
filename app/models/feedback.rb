dae1e2bcdb761bba6dfbc3611896f461224853f2
LinchSmyth <andrei95-1995@mail.ru>
2016-07-21 21:50:24 +0300

    Add Regexp for name field

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,8 +1,13 @@
 class Feedback < ActiveRecord::Base
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  VALID_NAME_REGEX = /\A[A-Z]+[a-z' ]{2,}+[A-Z][a-z']{1,}+\z/
+  # регулярное выражение пропускает только два слова разделенных пробелом
+  # оба слова должны начинаться с большой буквы, и иметь:
+  # Имя:      2+ буквы
+  # Фамилия:  2+ буквы
 
-  validates :name,      presence: true
+  validates :name,      presence: true, format:     { with: VALID_NAME_REGEX }
   validates :age,       presence: true, inclusion:  { within: 17..65, message: "is invalid: you must be from 17 to 65 years old" }
   validates :email,     presence: true, format:     { with: VALID_EMAIL_REGEX }
   validates :datetime,  presence: true
