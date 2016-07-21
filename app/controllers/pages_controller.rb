6b7721173977860c196e1f80fa9b190fce273e61
LinchSmyth <andrei95-1995@mail.ru>
2016-07-21 19:48:26 +0300

    Add Pages#create method with debugging line, change routes.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,15 @@ class PagesController < ApplicationController
 
   def about
   end
+
+  def create
+    Feedback.create(params[:person])
+    raise "Foo"
+  end
+
+  private
+
+  def feedback_params
+    params.require(:feedback).permit(:name, :email, :age, :datetime, :message)
+  end
 end
