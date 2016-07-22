9890aad51174a50e2335d5b9740c3dab86aecf00
LinchSmyth <andrei95-1995@mail.ru>
2016-07-22 15:59:17 +0300

    Add paperclip validations, fix age validations

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -8,8 +8,19 @@ class Feedback < ActiveRecord::Base
   # Фамилия:  2+ буквы
 
   validates :name,      presence: true, format:     { with: VALID_NAME_REGEX }
-  validates :age,       presence: true, inclusion:  { within: 17..65, message: "is invalid: you must be from 17 to 65 years old" }
   validates :email,     presence: true, format:     { with: VALID_EMAIL_REGEX }
+  validates :age,       inclusion:  { within: 17..65, message: "is invalid: you must be from 17 to 65 years old" }
   validates :datetime,  presence: true
+  # paperclip validations
+  has_attached_file    :attachment
+  validates_attachment :attachment,
+                       :content_type => {:content_type => %w(image/jpeg
+                                                             image/jpg
+                                                             image/png
+                                                             application/pdf
+                                                             application/msword
+                                                             application/vnd.openxmlformats-officedocument.wordprocessingml.document)
+                                        }
+
 
 end
